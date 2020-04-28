196b2899c8c26d0a62a09629cdbbcfe421203fda
vinamramunot-tech <munov01@pfw.edu>
2020-04-28 19:26:38 +0530

    checking css for padding for the container

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 
 $(document).ready(function(){
-    // $(myCarousel).carousel("pause");
-
-    $("#optionsButton").click(function(){
-        // $(this).attr({"data-toggle":"popover", 
-        //         "title":"Coming Soon", "data-content":"Amazing options coming soon"});
-        console.log("hello");
-    })
+    $(myCarousel).carousel("pause");
 })
